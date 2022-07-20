1089ee65f8f47de67b16d3bbf7922c0a13d601dd
Caleb Mutch <cmm2209@columbia.edu>
2022-07-20 18:29:45 -0400

    to end of React Capabilities

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import { render } from "react-dom";
 import SearchParams from "./SearchParams";
 import { StrictMode } from "react";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import Details from "./Details";
 
 const App = () => {
   return (
     <StrictMode>
       <div>
-        <h1>Adopt Me!</h1>
-        <SearchParams />
+        <BrowserRouter>
+          <header>
+            <Link to="/">Adopt Me!</Link>
+          </header>
+          ;
+          <Routes>
+            <Route path="/details/:id" element={<Details />} />
+            <Route path="/" element={<SearchParams />} />
+          </Routes>
+        </BrowserRouter>
+        ;
       </div>
     </StrictMode>
   );
